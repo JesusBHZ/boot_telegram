351c7d40016196da85ee1b368f28b3f8ecf3007c
Jesus Bautista <1721110061@utectulancingo.edu.mx>
2024-09-22 01:56:22 +0000

    data

--- api/get.py
+++ api/get.py
@@ -2,20 +2,22 @@ import requests
 import time
 import os
 
-API_URL = "http://localhost:8000/keep-alive"  # Cambia esto a tu URL de API
+API_URL = os.getenv("TELEGRAM_BOT_API")  # Cambia esto a tu URL de API
 
 def keep_alive():
     while True:
         try:
+            # Hacer una petición GET a tu API
             response = requests.get(API_URL)
             if response.status_code == 200:
-                print("API está despierta.")
+                print("API está activa, respuesta exitosa.")
             else:
-                print(f"Error al acceder a la API: {response.status_code}")
-        except Exception as e:
-            print(f"Error: {e}")
-        
-        time.sleep(600)  # Espera 10 minutos (600 segundos) antes de la siguiente solicitud
+                print(f"Error: {response.status_code}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error al hacer la petición: {e}")
+
+        # Esperar 5 minutos antes de la siguiente petición
+        time.sleep(200)  # 300 segundos = 5 minutos
 
 if __name__ == "__main__":
     keep_alive()
